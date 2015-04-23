1782271e0a90d0205eff6c3122859cbb52c8a20b
kas2207 <kas2207@columbia.edu>
2015-04-22 22:17:55 -0400

    Initial Commit: Finished Problem 1

---
+++ Prob_1.py
@@ -0,0 +1,16 @@
+def listnum(numberlist):
+	theSum = 0
+	for i in numberlist:
+		theSum += i
+	print theSum
+
+
+L= []
+for x in xrange(1,1000):
+	if x % 3 == 0:
+		L.append(x)
+	elif x % 5 == 0:
+		L.append(x)
+	else:
+		next
+listnum(L)
