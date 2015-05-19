c06a5bced2e1e304e7c7f195a6f975ae0ea0c9ab
kas2207 <kas2207@columbia.edu>
2015-05-19 16:53:38 -0400

    Initial commit

---
+++ 1_8.py
@@ -0,0 +1,9 @@
+'''
+Python allows negative integers to be used as indices into a sequence, such as 
+a string. If string s has length n, and expression s[k] is used for index 
+−n ≤ k < 0, what is the equivalent index j ≥ 0 such that s[j] references the 
+same element?
+'''
+
+def index_locate(string,len(string),negative_indice,positive_indice):
+	for 
