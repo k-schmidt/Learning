f88adcd772411a81b39b78a0af7c10e578dd8a7e
kas2207 <kas2207@columbia.edu>
2015-05-19 23:10:16 -0400

    Fixed and checked

--- 1_8.py
+++ 1_8.py
@@ -1,9 +1,18 @@
 '''
-Python allows negative integers to be used as indices into a sequence, such as 
-a string. If string s has length n, and expression s[k] is used for index 
-−n ≤ k < 0, what is the equivalent index j ≥ 0 such that s[j] references the 
-same element?
+Python allows negative integers to be used as indices into a sequence, 
+such as a string. If string s has length n, and expression s[k] is used for in- 
+dex -n<=k<0, what is the equivalent index j>=0such that s[j] references 
+the same element?
 '''
 
-def index_locate(string,len(string),negative_indice,positive_indice):
-	for 
+def index_locate(data, k):
+	idx = k - len(data)
+	if data:
+		print data[idx], idx
+		return data[idx], idx
+	else:
+		False
+
+if __name__ == '__main__':
+	index_locate([1,2,3,4,5], 4)
+	index_locate([1,2,3,4,5], 2)        
